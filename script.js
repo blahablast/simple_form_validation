8b062bf8d1737ac1b4112bdbfa302e0f427ecb58
blahablast <blahasam@gmail.com>
2023-04-30 12:49:41 -0400

    added some JS

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert('Hey')
+// Bring in everything from the DOM
+const form = document.querySelector('#form')
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const password2 = document.querySelector('#password2')
+
+// Show input error message
+showError = (input, message) => {
+  const formControl = input.parentElement
+  formControl.className = 'form-control error'
+  const small = formControl.querySelector('small')
+  small.innerText = message
+}
+
+// Adding an event listenter for when we submit
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  if (username.value === '') {
+    showError(username, 'Username is required')
+  } else {
+    showSuccess()
+  }
+})
